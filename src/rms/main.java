0d114fa2dd864db1e2a6e0c69232ac4eadbf4ffc
Cyrus Lau <cyruslau.work@gmail.com>
2023-04-10 21:47:28 +0800

    init

---
+++ src/rms/main.java
@@ -0,0 +1,32 @@
+package rms;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
+import javax.swing.*;
+import net.proteanit.sql.DbUtils;
+
+// LAU Ka Pui s226064
+// YOUR_NAME YOUR_SID
+// YOUR_NAME YOUR_SID
+// YOUR_NAME YOUR_SID
+
+public class main {
+
+	public static class ex {
+		public static int days = 0;
+	}
+
+	public static void main(String[] args) {
+//        login();
+//        create();
+	}
+}
